9b6601444c4a19e708c283fd42ddd8a4935b4cc1
Shubham <nhaldes@gmail.com>
2022-01-25 00:23:23 +0530

    Added: Single Player At Start Position 0

---
+++ UC1_GamePlayWithSingleplayer.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class UC1_GamePlayWithSingleplayer {
+
+	public static final int startPosition=0;
+	public static void main(String[] args){
+
+		System.out.println("Player at Start Position "+ startPosition);
+	}
+
+}
